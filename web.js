6e7a5489b06bb8d225b51f7cd44c75dd3f13e877
Phelipe <phelipewesleydeoliveira@gmail.com>
2018-05-26 18:53:59 -0300

    Adcionada comando para evitar dormir

--- web.js
+++ web.js
@@ -1,15 +1,4 @@
-var express = require('express')
-var packageInfo = require('./package.json')
-
-var app = express()
-
-app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version })
-})
-
-var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Web server started at http://%s:%s', host, port)
-})
+var http = require("http");
+setInterval(function() {
+    http.get("http://manjarogroupbot.herokuapp.com");
+}, 300000);
