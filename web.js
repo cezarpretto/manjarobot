6a34381b2cbae51a0c2ecf53756fe8ba8873031c
Phelipe <phelipewesleydeoliveira@gmail.com>
2018-05-26 19:04:49 -0300

    Adcionada comando para evitar dormir

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
 var http = require("http");
+
 setInterval(function() {
-    http.get("http://manjarogroupbot.herokuapp.com");
-}, 300000);
+  http.get("http://manjarogroupbot.herokuapp.com")
+  console.log("mensagem enviada!")
+}, 250000);
+
+handle = (req, res) -> res.end "hit"
+
+server = http.createServer handle
+
+server.listen process.env.PORT || 5000
