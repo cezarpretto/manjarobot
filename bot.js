3c0a44d9c31d4b6e4b21bf5d08f403f3a67c7360
Phelipe <phelipewesleydeoliveira@gmail.com>
2018-05-29 18:36:01 -0300

    Adicionado o comando ao menu

--- bot.js
+++ bot.js
@@ -253,7 +253,7 @@ function bestCommends(chatID) {
         console.log(text)
         
       }
-      bot.sendMessage(chatID, text, { parse_mode: 'Markdown' })
+      bot.sendMessage(chatID, String(text), { parse_mode: 'Markdown' })
       client.close()
     })
   })
