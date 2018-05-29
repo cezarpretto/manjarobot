c0d9d1cb64dc9308f3960639e4e40ff9e3e1543f
Phelipe <phelipewesleydeoliveira@gmail.com>
2018-05-28 22:14:40 -0300

    correção de bug

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 module.exports = {
-    token: '591408486:AAG-LhwYLrYOAHPO8SwYj7pHD18Ug9AD4OY',//'614598096:AAET4C9MomWzcFBiETK5wpZ1Pi6phXSU3ok',
+    token: '614598096:AAET4C9MomWzcFBiETK5wpZ1Pi6phXSU3ok',
     database: 'mongodb://manjarobot:manjarobot@ds237660.mlab.com:37660/manjarobot',
     dbName: 'manjarobot'
 }
+//'591408486:AAG-LhwYLrYOAHPO8SwYj7pHD18Ug9AD4OY',
